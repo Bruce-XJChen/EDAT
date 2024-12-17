645a396ac7f78e1a5d6f8839783c94229760c868
Hailong <67309433+Hailong919@users.noreply.github.com>
2024-12-17 22:08:34 +0800

    Add files via upload

---
+++ main_edat.py
@@ -0,0 +1,424 @@
+import torch
+import torch.distributed as dist
+import argparse
+from tqdm import tqdm
+import os
+from collections import OrderedDict
+from quantization import RandomQuantizer
+from quantization import NCQuantizer, KContractionQuantizer
+from attack import PGD
+from torch.autograd import Variable
+from dataset import Cifar, Cifar_EXT, ImageNet
+from models import PreActResNet18
+from utils import save_checkpoint, torch_accuracy, AvgMeter
+from torchvision.models import resnet50, resnet152
+from lamb import Lamb
+import os
+import timm
+import math
+import pandas as pd
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3'
+
+parser = argparse.ArgumentParser(description='distributed adversarial training')
+parser.add_argument('--dataset', default='cifar', choices=['cifar', 'cifarext', 'imagenet', 'tinyimagenet'],
+                    help='dataset cifar or imagenet')
+# parser.add_argument('--world-size', default=2, type=int,
+#                    help='number of nodes for distributed training')
+# parser.add_argument('--rank', default=-1, type=int,
+#                    help='node rank for distributed training')
+# parser.add_argument('--dist-url', default='tcp://172.31.233.228:30035',type=str,
+#                    help='url used to set up distributed training')
+parser.add_argument('--dist-backend', default='nccl', type=str,
+                    help='distributed backend')
+parser.add_argument('--batch-size', default=512, type=int,
+                    help='batch size')
+parser.add_argument('--num-epochs', default=100, type=int,
+                    help='total training epoch')
+parser.add_argument('--eval-epochs', default=10, type=int,
+                    help='eval epoch interval')
+parser.add_argument('--fast', action="store_true",
+                    help='whether to use fgsm')
+parser.add_argument('--wolalr', action="store_true", default=True,
+                    help='whether to train without layer-wise adptive learning rate')
+parser.add_argument('--lr', default=0.01, type=float,
+                    help='learning rates')
+parser.add_argument('--dataset-path', default='datasets', type=str,
+                    help='dataset folder')
+parser.add_argument('--output_txt', default='cifar100_bs512_datomd_random.txt', type=str,
+                    help='extra information for outputs dir')
+
+qt = RandomQuantizer()
+#qt = NCQuantizer()
+#qt = KContractionQuantizer()
+
+
+
+def distributed(param, rank, size):
+    quantization = True
+    if size == 1:
+        quantization = False
+    if quantization:
+        DEVICE = torch.device('cuda', rank)
+        cuda_grad = param.grad.to(DEVICE)
+        if qt.set == 'random':
+            q, norm = qt.quantize(cuda_grad)
+            #de_grad = qt.dequantize(q, norm)
+            #res = cuda_grad - de_grad
+            q_list = [torch.zeros_like(q) for _ in range(size)]
+            norm_list = [torch.zeros_like(norm) for _ in range(size)]
+            dist.all_gather(q_list, q)
+            dist.all_gather(norm_list, norm)
+            tmp = torch.zeros_like(cuda_grad)
+            for q, norm in zip(q_list, norm_list):
+                tmp += qt.dequantize(q, norm)
+        elif qt.set == 'natural':
+            nat = qt.quantize(cuda_grad)
+            #res = cuda_grad - nat
+            nat_list = [torch.zeros_like(nat) for _ in range(size)]
+            dist.all_gather(nat_list, nat)
+            tmp = torch.zeros_like(cuda_grad)
+            for nat in nat_list:
+                tmp += nat
+        else:
+            topk_values, topk_indices = qt.quantize(cuda_grad)
+            #comp = torch.zeros_like(cuda_grad)
+            #comp = comp.scatter_(-1, topk_indices, topk_values)
+            #res = cuda_grad - comp
+            vlist = [torch.zeros_like(topk_values) for _ in range(size)]
+            ilist = [torch.zeros_like(topk_indices) for _ in range(size)]
+            dist.all_gather(vlist, topk_values)
+            dist.all_gather(ilist, topk_indices)
+            tmp = torch.zeros_like(cuda_grad)
+            for v, i in zip(vlist, ilist):
+                zero_tensor = torch.zeros_like(cuda_grad)
+                tmp += zero_tensor.scatter_(dim=-1, index=i, src=v)
+        param.grad = tmp.to(DEVICE)
+    else:
+        dist.all_reduce(param.grad, op=dist.ReduceOp.SUM)
+    param.grad.data /= float(size)
+    #return res
+    
+def global_distributed(param, rank, size):
+    quantization = True
+    if size == 1:
+        quantization = False
+    if quantization:
+        b = 8    ### b is # of bits
+        s = torch.pow(torch.tensor(2), b) - 1    ### number of quantization levels
+        DEVICE = torch.device('cuda', rank)
+        cuda_grad = param.grad.to(DEVICE)
+        norm_square = pow(torch.norm(cuda_grad), 2).to(DEVICE)
+        dist.all_reduce(norm_square, op=dist.ReduceOp.SUM)
+        norm = math.sqrt(norm_square)
+        sign = torch.sign(cuda_grad)
+        grad_normalized = torch.abs(cuda_grad) / norm
+        l = torch.floor(grad_normalized * s)
+        p = (s * grad_normalized - l)
+        yi = (l + torch.distributions.binomial.Binomial(1, p).sample())
+        yi=yi.byte()
+        
+        sign_list = [torch.zeros_like(sign) for _ in range(size)]
+        yi_list = [torch.zeros_like(yi) for _ in range(size)]
+        dist.all_gather(sign_list, sign)
+        dist.all_gather(yi_list, yi)
+        tmp = torch.zeros_like(cuda_grad)
+        for sign, yi in zip(sign_list, yi_list):
+            tmp += sign * yi * norm / s
+        param.grad = tmp.to(DEVICE)
+    else:
+        dist.all_reduce(param.grad, op=dist.ReduceOp.SUM)
+    param.grad.data /= float(size)
+
+def fgsm(gradz, step_size):
+    return step_size * torch.sign(gradz)
+
+
+# global global_noise_data
+global_noise_data = torch.zeros([512, 3, 224, 224]).cuda()
+
+
+def train(net, data_loader, optimizer,
+          criterion, DEVICE=torch.device('cuda:0'),
+          descrip_str='', es=(8.0, 10), fast=False, lr_scheduler=None, warmup=False):
+    net.train()
+    pbar = tqdm(data_loader, ncols=200)
+    advacc = -1
+    advloss = -1
+    cleanacc = -1
+    cleanloss = -1
+    pbar.set_description(descrip_str)
+    size = dist.get_world_size()
+    rank = dist.get_rank()
+
+    eps, step = es
+    lookhead_dict = {n: torch.zeros_like(p, device=DEVICE) for n, p in net.named_parameters()}          
+    parameters_dict = {n: torch.zeros_like(p, device=DEVICE) for n, p in net.named_parameters()}   #save the previous param
+    if not fast:
+        at = PGD(eps=eps / 255.0, sigma=2 / 255.0, nb_iter=step, DEVICE=DEVICE)
+        for i, (data, label) in enumerate(pbar):
+            if i == 0:
+                for param in net.parameters():
+                    dist.broadcast(param.data, 0)
+                for name, param in net.named_parameters():
+                    parameters_dict[name].data.copy_(param.data)
+            
+            if i != 0:
+                #eta = lr_scheduler.get_lr()[0]
+                for name, param in net.named_parameters():
+                    if name in lookhead_dict:
+                        updated_value = parameters_dict[name].data - (eta * lookhead_dict[name].data)
+                        param.data.copy_(updated_value)     #omd forward step
+
+                        
+            data = data.to(DEVICE)
+            label = label.to(DEVICE)
+
+            optimizer.zero_grad()
+
+            pbar_dic = OrderedDict()
+
+            adv_inp = at.attack(net, data, label)
+            optimizer.zero_grad()
+            net.train()
+            pred = net(adv_inp)
+            loss = criterion(pred, label)
+
+            acc = torch_accuracy(pred, label, (1,))
+            advacc = acc[0].item()
+            advloss = loss.item()
+            (loss * 1.0).backward()
+
+            pred = net(data)
+
+            loss = criterion(pred, label)
+            loss.backward()
+            
+            
+            for name, param in net.named_parameters():
+                if param.grad is not None:
+                    lookhead_dict[name].data.copy_(param.grad.data)
+            if i==0:
+                continue
+            
+            for name, param in net.named_parameters():
+                distributed(param, rank, size, lookhead_dict[name])
+                #error_dict[name] = global_distributed(param, rank, size, lookhead_dict[name])
+                
+            for name, param in net.named_parameters():
+                param.data = parameters_dict[name].data     
+            optimizer.step()
+            for name, param in net.named_parameters():
+                parameters_dict[name].data.copy_(param.data)  
+            optimizer.step()
+            if warmup:
+                lr_scheduler.step()
+            acc = torch_accuracy(pred, label, (1,))
+            cleanacc = acc[0].item()
+            cleanloss = loss.item()
+            #if rank == 0:
+            #    f = open(os.path.join('outputs', 'loss_cifar100_datsgd_random.txt'), 'a')
+            #    f.write(str(cleanloss + advloss)+'\n')
+            #    f.close()
+            pbar_dic['standard test acc'] = '{:.2f}'.format(cleanacc)
+            pbar_dic['standard test loss'] = '{:.2f}'.format(cleanloss)
+            pbar_dic['robust acc'] = '{:.2f}'.format(advacc)
+            pbar_dic['robust loss'] = '{:.2f}'.format(advloss)
+            pbar_dic['lr'] = lr_scheduler.get_lr()[0]
+            pbar.set_postfix(pbar_dic)
+    else:
+        global global_noise_data
+        for i, (data, label) in enumerate(pbar):
+            data = data.to(DEVICE)
+            label = label.to(DEVICE)
+            global_noise_data.uniform_(-eps / 255.0, eps / 255.0)
+            # sync all parameters at epoch 0
+            if i == 0:
+                for param in net.parameters():
+                    dist.broadcast(param.data, 0)
+            for j in range(1):
+                noise_batch = Variable(global_noise_data[0:data.size(0)], requires_grad=True).cuda()
+                in1 = data + noise_batch
+                in1.clamp_(0, 1.0)
+                output = net(in1)
+                loss = criterion(output, label)
+
+                acc = torch_accuracy(output, label, topk=(1,))
+                cleanacc = acc[0].item()
+                cleanloss = loss.item()
+
+                loss.backward()
+
+                # Update the noise for the next iteration
+                pert = fgsm(noise_batch.grad, 1.25 * eps / 255.0)
+                global_noise_data[0:data.size(0)] += pert.data
+                global_noise_data.clamp_(-eps / 255.0, eps / 255.0)
+
+                # Dscend on the global noise
+                noise_batch = Variable(global_noise_data[0:data.size(0)], requires_grad=False).cuda()
+                in1 = data + noise_batch
+                in1.clamp_(0, 1.0)
+                output = net(in1)
+                loss = criterion(output, label)
+                optimizer.zero_grad()
+                loss.backward()
+                for param in net.parameters():
+                    print(torch.norm(param.grad).shape)
+                for param in net.parameters():
+                    distributed(param, rank, size)
+                optimizer.step()
+                if warmup:
+                    lr_scheduler.step()
+                # print(lr_scheduler.get_lr()[0])
+            pbar_dic = OrderedDict()
+            pbar_dic['Acc'] = '{:.2f}'.format(cleanacc)
+            pbar_dic['loss'] = '{:.2f}'.format(cleanloss)
+            pbar_dic['lr'] = lr_scheduler.get_lr()[0]
+            pbar.set_postfix(pbar_dic)
+    #norm_error = torch.tensor([tensor.norm(p=2) for tensor in error_dict.values()]).norm(p=2)
+    #if rank == 0:
+    #    f = open(os.path.join('outputs', 'norm_cifar100_datsgd_random.txt'), 'a')
+    #    f.write(str(norm_error)+'\n')
+    #    f.close()
+
+
+def eval(net, data_loader, DEVICE=torch.device('cuda:0'), es=(8.0, 20)):
+    net.eval()
+    pbar = tqdm(data_loader)
+    clean_accuracy = AvgMeter()
+    adv_accuracy = AvgMeter()
+
+    pbar.set_description('Evaluating')
+    eps, step = es
+    at_eval = PGD(eps=eps / 255.0, sigma=2 / 255.0, nb_iter=step, DEVICE=DEVICE)
+    for (data, label) in pbar:
+        data = data.to(DEVICE)
+        label = label.to(DEVICE)
+
+        with torch.no_grad():
+            pred = net(data)
+            acc = torch_accuracy(pred, label, (1,))
+            clean_accuracy.update(acc[0].item(), acc[0].size(0))
+
+        adv_inp = at_eval.attack(net, data, label)
+
+        with torch.no_grad():
+            pred = net(adv_inp)
+            acc = torch_accuracy(pred, label, (1,))
+            adv_accuracy.update(acc[0].item(), acc[0].size(0))
+
+        pbar_dic = OrderedDict()
+        pbar_dic['standard test acc'] = '{:.2f}'.format(clean_accuracy.mean)
+        pbar_dic['robust acc'] = '{:.2f}'.format(adv_accuracy.mean)
+        pbar.set_postfix(pbar_dic)
+
+    return clean_accuracy.mean, adv_accuracy.mean
+
+
+def main():
+    args = parser.parse_args()
+    print(args)
+    # dist.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
+    #                       world_size=args.world_size, rank=args.rank)
+    dist.init_process_group(backend=args.dist_backend, init_method='env://')
+    batch_size = args.batch_size
+    num_epochs = args.num_epochs
+    eval_epochs = args.eval_epochs
+    # DEVICE = torch.device('cuda:0')
+    world_size = dist.get_world_size()
+    rank = dist.get_rank()
+    torch.cuda.set_device(rank)
+    DEVICE = torch.device('cuda', rank)
+    criterion = torch.nn.CrossEntropyLoss().to(DEVICE)
+
+    global global_noise_data
+    if args.dataset == 'cifar' or args.dataset == 'cifarext':
+
+        global_noise_data = torch.zeros([batch_size, 3, 32, 32]).to(DEVICE)
+
+        #net = PreActResNet18()
+        net = resnet152(pretrained=True)
+        #nf = net.fc.in_features
+        #n_classes = 10
+        #fc = torch.nn.Linear(nf, n_classes)
+        #net.fc = fc
+        # net = torch.nn.DataParallel(net, device_ids=[0,1,2,3]).to(DEVICE)
+        net = torch.nn.parallel.DistributedDataParallel(net.cuda(), device_ids=[rank], output_device=rank).to(DEVICE)
+
+        if args.wolalr:
+            optimizer = torch.optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
+        else:
+            #optimizer = torch.optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
+            optimizer = Lamb(net.parameters(), lr=args.lr, weight_decay=1e-4, betas=(.9, .999), adam=False)
+        lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[75, 90, 95], gamma=0.1)
+
+        if args.dataset == 'cifar':
+            ds_train, ds_val, sp_train = Cifar.get_loader(batch_size, world_size, rank, args.dataset_path)
+        else:
+            ds_train, ds_val, sp_train = Cifar_EXT.get_loader(batch_size, world_size, rank, args.dataset_path)
+        es = (8.0, 10)
+
+    elif 'imagenet' in args.dataset:
+        global_noise_data = torch.zeros([batch_size, 3, 224, 224]).cuda()
+
+        #net = resnet50()
+        net = timm.create_model('deit_tiny_patch16_224', pretrained=True)
+        #net = timm.create_model('deit_small_patch16_224', pretrained=True)
+        net = torch.nn.parallel.DistributedDataParallel(net.cuda(), device_ids=[rank], output_device=rank).to(DEVICE)
+        #net = torch.nn.DataParallel(net, device_ids=[0, 1]).to(DEVICE)
+
+        if args.wolalr:
+            optimizer = torch.optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=1e-4)
+        else:
+            #optimizer = torch.optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=1e-4)
+            optimizer = Lamb(net.parameters(), lr=args.lr, weight_decay=1e-4, betas=(.9, .999), adam=False)
+        #lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[20, 25, 28], gamma=0.1)
+        lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[20, 25, 28], gamma=0.3)
+        
+        if args.dataset == 'imagenet':
+            root ='/public/datasets/imagenet/images'
+        elif args.dataset == 'tinyimagenet':
+            root ='/public/datasets/tiny-imagenet-200'
+        ds_train, ds_val, sp_train = ImageNet.get_loader(batch_size, world_size, rank, root)
+        es = (2.0, 4)
+
+    #lr_steps = 5 * len(ds_train)
+    #print('lr_step:{}'.format(lr_steps))
+    #lambda1 = lambda step: (step + 1) / lr_steps
+
+    #warm_up_lr_lchedule = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda1)
+
+    #
+    # lr_scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, cycle_momentum=False, base_lr=0, max_lr=0.15,
+    #         step_size_up=lr_steps / 2, step_size_down=lr_steps / 2)
+
+    #for epoch in range(5):
+        #
+    #    descrip_str = 'warmup epoch:{}/{}'.format(epoch, 5)
+    #    train(net, ds_train, optimizer, criterion, DEVICE,
+    #          descrip_str, es, fast=args.fast, lr_scheduler=warm_up_lr_lchedule, warmup=True)
+
+    
+    for epoch in range(num_epochs):
+
+        sp_train.set_epoch(epoch)
+
+        descrip_str = 'Training epoch:{}/{}'.format(epoch, num_epochs)
+        train(net, ds_train, optimizer, criterion, DEVICE,
+              descrip_str, es, fast=args.fast, lr_scheduler=lr_scheduler)
+
+        lr_scheduler.step()
+        eval_epochs = 1
+        if eval_epochs > 0 and (epoch + 1) % eval_epochs == 0:
+            cleanacc, advacc = eval(net, ds_val, DEVICE, es)
+            if rank == 0:
+                f = open(os.path.join('outputs', args.output_txt), 'a')
+                f.write(str(cleanacc)+' ')
+                f.write(str(advacc)+'\n')
+                f.close()
+
+    eval(net, ds_val, DEVICE, es)
+
+
+if __name__ == "__main__":
+    main()
